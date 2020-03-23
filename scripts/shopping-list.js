2cda95b10e8fefe587d5d0bfd3ad48f2cd030e34
MxJRd <reed182@mail.chapman.edu>
2020-03-23 14:43:39 -0700

    moved files to store.js

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -41,59 +41,6 @@ const render = function() {
   $('.js-shopping-list').html(shoppingListItemsString);
 };
 
-
-//findAndDelete
-function findAndDelete(id){
-  findByID(id);
-  this.items.filter((number) => number === id);
-}
-
-
-//findAndUpdateName
-function findAndUpdateName(id, newName){
-  try {
-    item.validateName(newName);
-    findByID(id);
-    render();
-    }
-   catch (error) {
-    console.log(`Cannot update name: ${error.message}`);
-  }
-}
-
-//findAndToggleChecked
-function findAndToggleChecked(id){
-  //this will become a cuid kajlshdkjsdfhasdf thing
-  this.findByID(id);
-  return store.hideCheckedItems = true;
-}
-
-
-
-
-//addItem function
-
-function addItem(name){
-  try {
-    item.validateName(name);
-    this.items.push(item.create(name));
-    console.log(store.items);
-    //render();
-  } catch (error) {
-    console.log("I sure love when instructions are specific.");
-  }
-}
-
-//findByID function
-
-function findByID(id){
-  let text = store.items.find(function(){
-    if(id === store.items.id){
-      return id;
-    }
-  });
-}
-
 const addItemToShoppingList = function(itemName) {
   try {
     item.validateName(itemName);
@@ -202,17 +149,5 @@ const bindEventListeners = function() {
 export default {
   render,
   bindEventListeners,
-  findByID,
-  findAndToggleChecked,
-  addItem,
-  findAndUpdateName,
-  findAndDelete
 };
 
-/*
-store = {
-  items,
-  hideCheckedItems,
-  foo
-}
-*/
--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,55 @@
-import item from './item.js';
-
-let items = [1, 2, 3];
+let items = [];
 let hideCheckedItems = false;
 
-export default {
-  items,
-  hideCheckedItems
-};
+//findAndDelete
+function findAndDelete(id){
+    findByID(id);
+    this.items.filter((number) => number === id);
+  }
+//findAndUpdateName
+function findAndUpdateName(id, newName){
+    try {
+      item.validateName(newName);
+      findByID(id);
+      render();
+      }
+     catch (error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  }
+//findAndToggleChecked
+function findAndToggleChecked(id){
+    //this will become a cuid kajlshdkjsdfhasdf thing
+    this.findByID(id);
+    return store.hideCheckedItems = true;
+}
+//addItem function
+function addItem(name){
+    try {
+      item.validateName(name);
+      this.items.push(item.create(name));
+      console.log(store.items);
+      //render();
+    } catch (error) {
+      console.log("I sure love when instructions are specific.");
+    }
+  }
+  
+//findByID function 
+function findByID(id){
+    let text = store.items.find(function(){
+      if(id === store.items.id){
+        return id;
+      }
+    });
+}
+export default
+{
+items,
+hideCheckedItems,
+findByID,
+findAndToggleChecked,
+addItem,
+findAndUpdateName,
+findAndDelete
+}
