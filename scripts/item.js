e3941a020c295557fe4827ffa5559a384e02591f
MxJRd <reed182@mail.chapman.edu>
2020-03-23 13:04:29 -0700

    nightmare fuel changes make head hurt

--- scripts/item.js
+++ scripts/item.js
@@ -1,3 +1,21 @@
-const foo = 'bar';
+/* eslint disable */
 
-export default {};
+function validateName(name){
+    if(!name){
+    throw new TypeError("Name must not be blank, peasant");
+    console.log('sup Im working');
+    }
+}
+function create(name){
+    let item = {
+      id: cuid(),
+      name: name,
+      checked:false
+ };
+  console.log(item);
+}
+export default {
+    validateName,
+    create
+};
+
